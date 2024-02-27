f026b7d5fd86b8544dadb293ab2990eb1f018415
Felipe Pereira <luisfelipelfsp3@gmail.com>
2024-02-26 21:38:44 -0300

    :sparkles: feat: SearchBar Component

---
+++ src/components/base/news/SearchBar.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { MdTune } from "react-icons/md";
+
+export default function SearchBar() {
+	return (
+		<div className="flex bg-white shadow-md p-3 items-center gap-5 rounded-lg">
+			<Input type="search" placeholder="Search Here..." className="bg-zinc-100/60 border-0 focus:ring-0" />
+			<Button className="rounded-lg bg-bdpurple hover:bg-bdpurple/90">
+				Search
+			</Button>
+			<div>
+				<MdTune color="#7A2572" size={27} />
+			</div>
+		</div>
+	);
+}
