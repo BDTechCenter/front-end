dda50ac664f85db4b80553d14a649558354a6cad
Felipe Pereira <luisfelipelfsp3@gmail.com>
2024-02-26 21:38:25 -0300

    :lipstick: refactor: Just color navbar

--- src/app/news/[idNews]/page.tsx
+++ src/app/news/[idNews]/page.tsx
@@ -6,6 +6,7 @@ import NewsContent from "@/components/base/news/NewsContent";
 export default function NewsContentPage() {
 	return (
 		<main>
+			<NavBar variant="white" />
 			<NewsContent />
 		</main>
 	);
