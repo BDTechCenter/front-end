4828983de888f103abfe2e89df6db4b602088aef
joaop-ribeiro <joao06.ribeiro.costa@gmail.com>
2024-02-27 09:16:27 -0300

    fix: update banner news page

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -2,16 +2,21 @@ import NavBar from "@/components/base/common/NavBar";
 import TopBanner from "@/components/base/common/TopBanner";
 import NewsList from "@/components/base/news/NewsList";
 import SearchBar from "@/components/base/news/SearchBar";
+import { Button } from "@/components/ui/button";
 
 export default function NewsPage() {
 	return (
 		<main className="w-full h-full">
 			<NavBar variant="white" />
 			<div className="relative">
-				<TopBanner>
-					Follow the main <span className="text-bdlightpurple">news</span> of
-					the moment...
-				</TopBanner>
+			<TopBanner  text={bannerNews.text} className="flex w-1/2 justify-center items-center">
+        <Button
+              variant="bdlight"
+              className="rounded-lg w-1/3 text-xl font-bold py-6 2xl:text-2xl"
+            >
+              teste
+        </Button>
+      </TopBanner>
 				<div className="w-[70%] mx-auto absolute left-0 right-0 -bottom-7 z-10">
 					<SearchBar />
 				</div>
@@ -22,3 +27,15 @@ export default function NewsPage() {
 		</main>
 	);
 }
+
+export const bannerNews = {
+	text:(
+		<p>
+			Follow the main <span className="text-bdlightpurple">news</span> of
+			the moment...
+		</p>
+	)
+}
+
+
+
