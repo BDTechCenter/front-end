38ffdf4fc40ff9f43d9292ac68953cd7e18d3c9e
joaop-ribeiro <joao06.ribeiro.costa@gmail.com>
2024-03-06 13:38:56 -0300

    fix: merging the home page and introduction page types

--- src/api/home/types.ts
+++ src/api/home/types.ts
@@ -17,3 +17,40 @@ export interface News {
 export interface ButtonText {
   text: string
 }
+
+export interface IntroductionProps {
+  bannerSection: BannerSection;
+  features: Feature[];
+  bannerIntroduction: BannerIntroduction;
+  cardTeam: CardTeamMember[];
+}
+
+interface BannerSection {
+  title: string;
+  titleArea: string;
+  subtitle: string;
+  buttonTxt: string;
+}
+
+export interface Feature {
+  title: string;
+  icon: JSX.Element;
+  content: string;
+  orientation?: string;
+}
+
+interface BannerIntroduction {
+  link: string;
+  text: string;
+}
+
+export interface CardTeamMember {
+  name: string;
+  img: string;
+  functionTeam: string;
+  url: string
+}
+
+
+
+
---
+++ src/components/base/radar/informationRadar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function informationRadar() {
+  return (
+    <div>
+      <h1></h1>
+      <p></p>
+    </div>
+  )
+}
