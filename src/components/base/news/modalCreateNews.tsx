b8a2ac2d7350d17bd2cc524749fd20939120f832
joaop-ribeiro <joao06.ribeiro.costa@gmail.com>
2024-03-11 10:39:21 -0300

    feat: modal create news completed

--- src/components/base/news/modalCreateNews.tsx
+++ src/components/base/news/modalCreateNews.tsx
@@ -1,6 +1,6 @@
 import { Input } from "@/components/ui/input";
-import { useEffect } from "react";
-import ReactDOM from "react-dom";
+import InputTextEdit from "./InputTextEdit";
+import { Button } from "@/components/ui/button";
 
 interface ModalCreateNewsProps {
   isOpen?: boolean;
@@ -10,13 +10,21 @@ interface ModalCreateNewsProps {
 export default function ModalCreateNews({ setIsOpen, isOpen }: ModalCreateNewsProps){
  return (
   <div className="flex justify-center items-center w-full">
-    <div className="h-[60rem] w-[45rem] border p-12">
+    <div className="w-[45rem] border p-10">
       <h1 className="font-semibold text-md">Title</h1>
       <Input className="h-10 pl-7 mb-5 text-sm" placeholder="Add Title"/>
-      <h1 className="font-semibold text-md">Description</h1>
-      <Input className="h-20 pl-7 mb-5 text-sm" placeholder="Add Description"/>
       <h1 className="font-semibold text-md">Tag</h1>
-      <Input className="h-20 pl-7 mb-5 text-sm" placeholder="Add Description"/>
+      <Input className="h-10 pl-7 mb-5 text-sm" placeholder="Add Description"/>
+      <h1 className="font-semibold text-md">Content</h1>
+      <InputTextEdit/>
+      <div className="flex justify-end items-center">
+      <Button
+        variant="bdpurple"
+        className="rounded-md text-xl font-medium py-6"
+      >
+        teste			
+      </Button>
+      </div>
     </div>
   </div>
  )
