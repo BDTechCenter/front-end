ecadaba475ca51b2cb2c755d8a6e171c05d3418c
= <=>
2024-03-19 13:50:29 -0300

    fix: take tags

--- src/components/base/modal/ModalFilter.tsx
+++ src/components/base/modal/ModalFilter.tsx
@@ -9,11 +9,13 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 import { MdTune } from "react-icons/md";
 import InputTags from "../common/InputTags";
 
 
 export default function ModalFilter() {
+  const [tags, setTags] = useState<string[]>([])
   return (
     <Dialog>
       <DialogTrigger className="flex h-full justify-center items-center"><MdTune color="#7A2572" size={27} /></DialogTrigger>
@@ -28,10 +30,10 @@ export default function ModalFilter() {
         </DialogHeader>
           <div className="w-full">
             <h1 className="font-semibold text-md">Tags</h1>
-            <InputTags variant="wrap"/>
+            <InputTags onChange={setTags} variant="wrap"/>
             <DialogFooter className="flex w-full justify-end items-end">
               <DialogClose asChild>
-                <Button type="submit" onClick={()=>{}} className="rounded-sm shadow-md p-5 font-semibold text-lg " variant={"bdlight"}>
+                <Button type="submit" onClick={()=>{console.log(tags)}} className="rounded-sm shadow-md p-5 font-semibold text-lg " variant={"bdlight"}>
                   Filter
                 </Button>
               </DialogClose>
