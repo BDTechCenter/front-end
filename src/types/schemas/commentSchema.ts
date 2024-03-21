00edeb8504bb99ebcfa98f9d7ea88a8af812fd20
Flepsz <luisfelipelfsp3@gmail.com>
2024-03-21 10:43:04 -0300

    :sparkles: feat: Improved validation schema

--- src/types/schemas/commentSchema.ts
+++ src/types/schemas/commentSchema.ts
@@ -3,5 +3,6 @@ import z from "zod";
 export const commentSchema = z.object({
 	content: z
 		.string({ required_error: "Comment is required" })
-		.min(3, { message: "Your comment must be at least 3 characters." }),
+		.min(10, { message: "Your comment must be at least 10 characters." })
+		.max(500, { message: "Comments can be a maximum of 500 characters." }),
 });
--- src/types/schemas/newsShema.ts
+++ src/types/schemas/newsShema.ts
@@ -10,17 +10,23 @@ const ACCEPTED_IMAGE_TYPES = [
 
 export const newsSchema = z.object({
 	poster: z
-		.instanceof(File, { message: "The poster must be a image" })
+		.instanceof(File, { message: "The poster must be an image file." })
 		.refine(
 			(file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
 			"Only .jpg, .jpeg, .png and .webp formats are supported."
 		)
 		.refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+		.nullable()
 		.optional(),
-
-	title: z.string().min(3, { message: "Title must be at least 2 characters." }),
-	tags: z.array(z.string()).optional(),
+	title: z
+		.string()
+		.min(3, { message: "Title must be at least 3 characters." })
+		.max(29, { message: "Title must be a maximum of 30 characters." }),
+	tags: z
+		.array(z.string().max(30, { message: "Max characters of tags is 30." }))
+		.max(7, { message: "You can add up to 7 tags." })
+		.optional(),
 	content: z
-		.string({ required_error: "Content is required" })
-		.min(3, { message: "Content must be at least 3 characters." }),
+		.string({ required_error: "Content is required." })
+		.min(10, { message: "Content must be at least 10 characters." }),
 });
