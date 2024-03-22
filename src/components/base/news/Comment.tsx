197dbab98a5f41ec952bc450cf565e9d9cee5806
Flepsz <luisfelipelfsp3@gmail.com>
2024-03-22 10:23:43 -0300

    :lipstick: feat: Reduced border radius

--- src/components/base/news/Comment.tsx
+++ src/components/base/news/Comment.tsx
@@ -6,7 +6,7 @@ interface CommentProps {
 
 export default function Comment({ data }: CommentProps) {
 	return (
-		<div className="flex flex-col w-[80%] border border-bdlightpurple/20 rounded-md p-4 gap-1">
+		<div className="flex flex-col w-[80%] border border-bdlightpurple/20 rounded-sm p-4 gap-1">
 			<h1 className="text-md 2xl:text-lg font-bold">{data.author}</h1>
 			<p className="text-[0.75rem] 2xl:text-sm">{data.comment}</p>
 		</div>
