ba1ebd5416d5ac2248a573b85c1916a54362fe0e
Flepsz <luisfelipelfsp3@gmail.com>
2024-03-22 10:22:35 -0300

    :sparkles: feat: News size req dynamic

--- src/api/hooks/home/queries.ts
+++ src/api/hooks/home/queries.ts
@@ -1,12 +1,19 @@
-import api from "../../../services/api"
-import { useQuery } from "@tanstack/react-query"
-import { ContentNews} from "@/api/types/news/type"
+import api from "../../../services/api";
+import { useQuery } from "@tanstack/react-query";
+import { ContentNews } from "@/api/types/news/type";
 
 async function getMainNews() {
-  const { data } = await api.get<ContentNews>("news/preview?sortByView=true&size=3")
-  return data
+	const SIZE = 3;
+	const { data } = await api.get<ContentNews>(
+		`news/preview?sortByView=true&size=${SIZE}`
+	);
+
+	return data;
 }
 
 export function useFetchGetMainNews() {
-  return useQuery<ContentNews, Error>({queryKey:['news'], queryFn: getMainNews})
+	return useQuery<ContentNews, Error>({
+		queryKey: ["news"],
+		queryFn: getMainNews,
+	});
 }
