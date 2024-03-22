33a4ac0cdf5f28b7d665772acaf2f536b7a11f5f
Flepsz <luisfelipelfsp3@gmail.com>
2024-03-22 10:22:57 -0300

    :sparkles: feat: Dynamic Navbar color and data

--- src/components/base/common/NavBar.tsx
+++ src/components/base/common/NavBar.tsx
@@ -1,69 +1,83 @@
-import Image from 'next/image'
-import Link from 'next/link';
-import { cn } from '@/lib/utils';
-import { usePathname} from 'next/navigation';
-import { useState, useEffect } from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 interface NavBarProps {
   variant: "black" | "white";
 }
 
-export default function NavBar({variant}: NavBarProps) {
-  const [text, setText]= useState('')
-  const patch = usePathname()
+const textColors = {
+  black: {
+    text: "text-darkforeground",
+    noFocus: "text-darkforeground/60",
+    hover: "hover:text-darkforeground/80",
+  },
+  white: {
+    text: "text-foreground",
+    noFocus: "text-foreground/60",
+    hover: "hover:text-foreground/80",
+  },
+};
 
-  useEffect(()=>{
-    switch(variant){
-      case "black":{
-        setText('text-white')
-        break
-      }
-      case 'white':{
-        setText('text-foreground/60')
-        break
-      }
-      default:{
-        break
-      }
-    }
-  }, [variant, patch])
+const logoImages = {
+  black: "/bsh_logo_white.svg",
+  white: "/bsh_logo_black_red.svg",
+};
+
+export default function NavBar({ variant }: NavBarProps) {
+  const pathname = usePathname();
+  const { text, noFocus, hover } = textColors[variant];
+  const logoSrc = logoImages[variant];
 
   return (
-    <nav className={`flex flex-col ${variant === "black" ? 'bg-bddarkgray' : 'bg-white'}`}>
+    <div
+      className={`flex flex-col ${
+        variant === "black" ? "bg-bddarkgray" : "bg-white"
+      }`}
+    >
       <Image
-				alt="Bosch Supergraphic"
-				src="/supergraphic.svg"
-				width="1000"
-				height="1000"
-				className="w-screen"
-			/>
-     <div className='flex flex-row gap-5 items-center'>
-      <Link href="/" className='w-fit'>
-          {variant === "black" ? (
-            <Image
+        alt="Bosch Supergraphic"
+        src="/supergraphic.svg"
+        width="1000"
+        height="1000"
+        className="w-screen"
+      />
+      <nav className="flex flex-row gap-5 items-center">
+        <Link href="/" className="w-fit">
+          <Image
             alt="Bosch Logo White"
-            src="/bsh_logo_white.svg"
+            src={logoSrc}
             width="0"
             height="0"
             className="h-12 w-fit"
           />
-          ) : (
-            <Image
-            alt="Bosch Logo Color"
-            src="/bsh_logo_black_red.svg"
-            width="0"
-            height="0"
-            className="h-12 w-fit"
-          />
-          )}
         </Link>
-        <Link href={"/"}>
-            <h1 className={cn(`text-sm font-medium ${text}`, patch === "/" ? "text-bdlightpurple" : `hover:text-bdlightpurple ${text}`)}>Home</h1>
-        </Link>
-        <Link href={"/news"}>
-            <h1 className={cn(`text-sm font-medium ${text}`, patch === "/news" ? "text-bdlightpurple" : `hover:text-bdlightpurple ${text}`)}>News</h1>
-        </Link>
-     </div>
-    </nav>
-  )
+        {navLinks.map((link) => (
+          <Link
+            key={link.href}
+            className={cn(
+              "text-sm font-medium transition-colors",
+              hover,
+              pathname === link.href ? text : noFocus
+            )}
+            href={link.href}
+          >
+            {link.name}
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
 }
+
+const navLinks = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "News",
+    href: "/news",
+  },
+];
