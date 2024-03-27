52f558b6f03ffbf6ff3da6b8e3988fa51ecb0a02
joaop-ribeiro <joao06.ribeiro.costa@gmail.com>
2024-03-27 09:04:57 -0300

    feat: function like news

--- src/components/base/news/LikeForNews.tsx
+++ src/components/base/news/LikeForNews.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { BiLike } from "react-icons/bi";
 import { BiSolidLike } from "react-icons/bi";
+import { msalInstance } from "@/lib/sso/msalInstance";
 import { useState } from 'react';
 
 export default function LikeForNews() {
+  const token = msalInstance.acquireTokenByCode
   const [like, setLike] = useState(false)
+
+  const likePress = () =>{
+    console.log(token)
+    setLike(true)
+  }
 
   return (
     <div className="w-full items-center bg-transparent">
         { !like ? 
-            <BiLike size={22} color="#262626" onClick={()=>{setLike(true)}} className='cursor-pointer'/> 
+            <BiLike size={22} color="#262626" onClick={likePress} className='cursor-pointer'/> 
           :
             <BiSolidLike size={22} color="#262626" className='cursor-pointer'/>
         }
