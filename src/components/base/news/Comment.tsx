da06e63b0948f64636eeee7cf1e66f2139e2f409
Flepsz <luisfelipelfsp3@gmail.com>
2024-03-27 10:59:00 -0300

    :sparkles: feat: Content comment rendering HTML

--- src/components/base/news/Comment.tsx
+++ src/components/base/news/Comment.tsx
@@ -8,7 +8,11 @@ export default function Comment({ data }: CommentProps) {
 	return (
 		<div className="flex flex-col w-[80%] border border-bdlightpurple/20 rounded-sm p-4 gap-1">
 			<h1 className="text-md 2xl:text-lg font-bold">{data.author}</h1>
-			<p className="text-[0.75rem] 2xl:text-sm">{data.comment}</p>
+			<div
+				className="text-[0.75rem] 2xl:text-sm"
+				style={{ wordWrap: "break-word" }}
+				dangerouslySetInnerHTML={{ __html: data.comment }}
+			></div>
 		</div>
 	);
 }
