e154176d3265a8794bdc9ed84bcc6f765abb6584
Flepsz <luisfelipelfsp3@gmail.com>
2024-04-01 16:50:53 -0300

    :sparkles: feat: Added Token interceptor

--- src/services/tokenInterceptor.ts
+++ src/services/tokenInterceptor.ts
@@ -1,7 +1,5 @@
 import getMsalToken from "@/lib/sso/getMsalToken";
-import axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
-
-const hostURL = process.env.NEXT_PUBLIC_API_HOST;
+import { InternalAxiosRequestConfig } from "axios";
 
 interface TokenInfo {
 	token: string | null;
@@ -10,7 +8,7 @@ interface TokenInfo {
 
 let cachedToken: TokenInfo | null = null;
 
-const tokenInterceptor = async (
+export const tokenInterceptor = async (
 	config: InternalAxiosRequestConfig<any>
 ): Promise<InternalAxiosRequestConfig<any>> => {
 	if (!cachedToken || Date.now() > cachedToken.expiration) {
@@ -21,11 +19,3 @@ const tokenInterceptor = async (
 	config.headers.Authorization = `Bearer ${cachedToken?.token}`;
 	return config;
 };
-
-const api: AxiosInstance = axios.create({
-	baseURL: `${hostURL}/tech-news/`,
-});
-
-api.interceptors.request.use(tokenInterceptor);
-
-export default api;
