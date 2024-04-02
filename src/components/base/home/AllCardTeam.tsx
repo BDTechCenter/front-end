ab50ae8ae0a4d1aa2aaad4f4a8621683630a0a36
Flepsz <luisfelipelfsp3@gmail.com>
2024-04-02 16:55:47 -0300

    :sparkles: feat: CardTeam Responsive

--- src/components/base/home/AllCardTeam.tsx
+++ src/components/base/home/AllCardTeam.tsx
@@ -1,15 +1,23 @@
 import CardTeam from "./CardTeam";
 
 export default function AllCardTeam() {
+	const getInitial = (name: string) => {
+		const [firstName, lastName] = name.split(" ");
+		const firstLetterOfFirstName = firstName.charAt(0);
+		const firstLetterOfLastName = lastName.charAt(0);
+		return firstLetterOfFirstName + firstLetterOfLastName;
+	};
+
 	return (
 		<section className="flex flex-col gap-12">
 			<div className="flex w-full justify-center items-center">
 				<h1 className="font-semibold text-2xl 2xl:text-3xl">Dev Team</h1>
 			</div>
-			<div className="grid grid-rows-2 grid-flow-col gap-6 w-full items-center justify-center">
-				{teamData?.map((team) => (
-					<CardTeam data={team} />
-				))}
+			<div className=" mx-auto gap-6 max-w-[80%] w-full items-center justify-center sm:grid sm:grid-cols-1 xl:grid-cols-2 xl:max-w-[50%]">
+				{teamData?.map((team) => {
+					const fallback = getInitial(team.name);
+					return <CardTeam key={team.url} data={team} fallback={fallback} />;
+				})}
 			</div>
 		</section>
 	);
@@ -18,7 +26,7 @@ export default function AllCardTeam() {
 const teamData = [
 	{
 		name: "João Pedro",
-		img: "/TeamPedrinho.png",
+		img: "https://github.com/joaop-ribeiro.png",
 		function: "Front-End",
 		url: "https://github.com/joaop-ribeiro",
 	},
--- src/components/base/home/CardTeam.tsx
+++ src/components/base/home/CardTeam.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 interface CardTeamProps {
 	data: {
@@ -9,26 +9,28 @@ interface CardTeamProps {
 		function: string;
 		url: string;
 	};
+	fallback: string;
 }
 
-export default function CardTeam({ data }: CardTeamProps) {
+export default function CardTeam({ data, fallback }: CardTeamProps) {
 	return (
 		<div
-			className="flex flex-row gap-4 w-96 h-32 border-[1px] rounded-sm border-black/10 cursor-pointer"
+			className="flex gap-4 w-full h-full pr-32 border-[1px] rounded-sm border-black/10 cursor-pointer group transition-all hover:drop-shadow-md"
 			onClick={() => window.open(`${data?.url}`)}
 		>
-			<div className="h-full w-4 bg-bdpurple rounded-l-sm"></div>
-			<div className="flex w-full items-center justify-start gap-5">
-				<Image
-					className="w-20 h-2w-20 rounded-full"
-					src={data?.img}
-					alt="BD Squares"
-					height={100}
-					width={100}
-				/>
+			<div className="w-4 bg-bdpurple rounded-l-sm transition-all group-hover:opacity-70"></div>
+			<div className="flex w-full my-5 items-center justify-start gap-5 transition-all group-hover:opacity-70 ">
+				<Avatar className="size-14">
+					<AvatarImage
+						className="size-full rounded-full"
+						src={data?.img}
+						alt={`${data.name}'s Photo`}
+					/>
+					<AvatarFallback>{fallback}</AvatarFallback>
+				</Avatar>
 				<div className="flex flex-col">
-					<h1 className="text-2xl font-medium">{data?.name}</h1>
-					<h2 className="text-xl text-bdlightpurple font-medium">
+					<h1 className="text-xl font-medium">{data?.name}</h1>
+					<h2 className="text-lg text-bdlightpurple font-medium">
 						{data?.function}
 					</h2>
 				</div>
