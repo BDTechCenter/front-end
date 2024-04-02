40352e760c87b22cc8c5359233c91e4a12cda9d8
joaop-ribeiro <joao06.ribeiro.costa@gmail.com>
2024-04-02 14:36:35 -0300

    feat: like for comments

---
+++ src/components/base/news/LikeForComment.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { BiSolidUpvote } from "react-icons/bi";
+import { BiUpvote } from "react-icons/bi";
+import { msalInstance } from "@/lib/sso/msalInstance";
+import { useState } from "react";
+import { useMutationPatchCommentUpvote } from "@/api/hooks/news/queries";
+import { toast } from "react-toastify";
+
+interface LikeForNewsProps {
+	id: number;
+	alreadyUpVoted: boolean;
+}
+
+export default function LikeForComment({
+	id,
+	alreadyUpVoted,
+}: LikeForNewsProps) {
+	const { mutate } = useMutationPatchCommentUpvote();
+
+	const user = msalInstance.getActiveAccount();
+	const token = user ? user.idToken : "";
+	const [like, setLike] = useState(alreadyUpVoted);
+
+	const useLikePress = async () => {
+		if (!alreadyUpVoted) {
+			mutate(
+				{ id: id, token: token },
+				{
+					onSuccess: (data) => {
+						toast.success("upvote successfully", {
+							position: "top-right",
+							autoClose: 3000,
+							hideProgressBar: false,
+							closeOnClick: true,
+							pauseOnHover: true,
+							draggable: true,
+
+							progress: undefined,
+							theme: "light",
+						});
+					},
+					onError: (error) => {
+						toast.error(error.message, {
+							position: "top-right",
+							autoClose: 3000,
+							hideProgressBar: false,
+							closeOnClick: true,
+							pauseOnHover: true,
+							draggable: true,
+							progress: undefined,
+							theme: "light",
+						});
+					},
+				}
+			);
+			setLike(true);
+		}
+	};
+
+	return (
+		<div className="w-full items-center bg-transparent">
+			{!like ? (
+				<BiUpvote
+					size={18}
+					color="#262626"
+					onClick={useLikePress}
+					className="cursor-pointer"
+				/>
+			) : (
+				<BiSolidUpvote
+					size={18}
+					onClick={useLikePress}
+					color="#262626"
+					className="cursor-pointer"
+				/>
+			)}
+		</div>
+	);
+}
