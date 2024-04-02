02abb792b8849826131d89e2fdd64d7721de88a5
joaop-ribeiro <joao06.ribeiro.costa@gmail.com>
2024-04-02 14:54:53 -0300

    :feat: add token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,31 @@
-import axios from "axios";
+import getMsalToken from "@/lib/sso/getMsalToken";
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
 
 const hostURL = process.env.NEXT_PUBLIC_API_HOST;
 
-const api = axios.create({
-	baseURL: `${hostURL}/tech-news/`
+interface TokenInfo {
+	token?: string;
+	expiration: number;
+}
+
+let cachedToken: TokenInfo | null = null;
+
+const tokenInterceptor = async (
+	config: InternalAxiosRequestConfig<any>
+): Promise<InternalAxiosRequestConfig<any>> => {
+	if (!cachedToken || Date.now() > cachedToken.expiration) {
+		let { token, idToken } = await getMsalToken();
+		// Refresh token if expired or not cached
+		cachedToken = { token: idToken, expiration: Date.now() + 3600000 };
+	}
+	config.headers.Authorization = `Bearer ${cachedToken?.token}`;
+	return config;
+};
+
+const api: AxiosInstance = axios.create({
+	baseURL: `${hostURL}/tech-news/`,
 });
+
+api.interceptors.request.use(tokenInterceptor);
 
 export default api;
