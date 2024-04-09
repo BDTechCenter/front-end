3f733599abb1e62291a14a4f1a83bb973b6bfd74
Flepsz <luisfelipelfsp3@gmail.com>
2024-04-09 10:42:11 -0300

    :sparkles: feat: Added RefForward

--- src/components/base/common/InputTags.tsx
+++ src/components/base/common/InputTags.tsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState, KeyboardEvent } from "react";
+import {
+	useState,
+	ForwardRefRenderFunction,
+	forwardRef,
+} from "react";
 import { IoMdClose } from "react-icons/io";
 import { cn } from "@/lib/utils";
 import toast from "react-hot-toast";
@@ -10,8 +14,11 @@ export interface InputTagsProps {
 	onChange: (value: string[]) => void;
 }
 
-export default function InputTags({ variant, onChange }: InputTagsProps) {
-	const [value, setValue] = useState('')
+const InputTags: ForwardRefRenderFunction<HTMLInputElement, InputTagsProps> = (
+	{ variant, onChange },
+	ref
+) => {
+	const [value, setValue] = useState("");
 	const [tags, setTags] = useState<string[]>([]);
 	const MaxTegs = 7;
 
@@ -20,13 +27,13 @@ export default function InputTags({ variant, onChange }: InputTagsProps) {
 	};
 
 	const addTags = () => {
-		if (tags.length < MaxTegs && value !== '') {
+		if (tags.length < MaxTegs && value !== "") {
 			setTags([...tags, value]);
 			onChange([...tags, value]);
 		} else {
 			errorToast();
 		}
-		setValue('')
+		setValue("");
 	};
 
 	const removeTags = (index: number) => {
@@ -37,11 +44,25 @@ export default function InputTags({ variant, onChange }: InputTagsProps) {
 	return (
 		<div>
 			<div className="w-full flex flex-row gap-3">
-				<Input placeholder="Search your tags..." maxLength={35} value={value} onChange={setValue} />
-				<Button type="button" className="rounded-sm bg-bdpurple hover:bg-bdpurple/90" onClick={addTags}>tag</Button>
+				<Input
+					placeholder="Search your tags..."
+					maxLength={35}
+					value={value}
+					onChange={setValue}
+				/>
+				<Button
+					type="button"
+					className="rounded-sm bg-bdpurple hover:bg-bdpurple/90"
+					onClick={addTags}
+				>
+					tag
+				</Button>
 			</div>
 			<div
-				className={cn("flex p-1 gap-2 items-center my-5 overflow-y-hidden overflow-x-scroll", variant === "wrap" ? "flex-wrap" : "flex-row")}
+				className={cn(
+					"flex p-1 gap-2 items-center my-5 overflow-y-hidden overflow-x-scroll",
+					variant === "wrap" ? "flex-wrap" : "flex-row"
+				)}
 			>
 				{tags.map((tag, index) => (
 					<span
@@ -60,4 +81,6 @@ export default function InputTags({ variant, onChange }: InputTagsProps) {
 			</div>
 		</div>
 	);
-}
+};
+
+export default forwardRef(InputTags);
