c9024b463054d942826e09f5727b6cac6ece21d3
joaop-ribeiro <joao06.ribeiro.costa@gmail.com>
2024-04-10 16:42:29 -0300

    :feat: create type new form

--- src/api/types/all/type.ts
+++ src/api/types/all/type.ts
@@ -21,3 +21,27 @@ export interface FilterPath {
 	searchParams: ReadonlyURLSearchParams;
 	router: AppRouterInstance;
 }
+
+export interface AlertType {
+	variantButton:
+		| "delete"
+		| "ghost"
+		| "bdpurple"
+		| "bdlight"
+		| "default"
+		| "outline";
+	nameButton: string | any;
+	title: string;
+	description: string;
+	nameButtonAction: string;
+	Action: () => void;
+}
+
+export interface FormType {
+	title: string | any;
+	open: boolean;
+	openUpdate?: boolean;
+	OnSubmit: (values: any) => void;
+	idForm?: string;
+	idNews?: string;
+}
