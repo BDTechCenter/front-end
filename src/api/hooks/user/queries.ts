f53bbce10385acd0d1ca742b99e1fec8dbedc8be
joaop-ribeiro <joao06.ribeiro.costa@gmail.com>
2024-04-22 13:19:02 -0300

    :fix: update cache

--- src/api/hooks/user/queries.ts
+++ src/api/hooks/user/queries.ts
@@ -1,13 +1,14 @@
 import { ContentNews, News } from "@/api/types/news/type";
 import api from "@/services/api";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { title } from "process";
 import toast from "react-hot-toast";
 
 // News
 // GET user news
 async function getUserNews() {
 	const { data } = await api.get<ContentNews>(`news/author?sortBy=published`);
-	console.log(data)
+	console.log(data);
 	return data;
 }
 
@@ -48,12 +49,25 @@ export function useMutationPatchNews() {
 	return useMutation({
 		mutationFn: patchNews,
 		onSuccess: (_, variables) => {
-			queryClient.setQueryData(
-				["updateNews", variables.id],
-				(data: News[]) => {
-					console.log(data)
-				}
-			);
+			queryClient.setQueryData(["newsRead", variables.id], (data: News) => {
+				const newsEdited: any = {};
+				variables.newsObject.forEach((value, key) => (newsEdited[key] = value));
+				const newData = { ...data, ...newsEdited };
+
+				return [newData];
+			});
+			queryClient.setQueryData(["news"], (data: ContentNews) => {
+				const updatedNews = data.content.map((news) => {
+					if (news.id === variables.newsObject.get("id")) {
+						const updatedTitle = variables.newsObject.get("title");
+						if (updatedTitle) {
+							return { ...news, title: updatedTitle };
+						}
+					}
+					return news;
+				});
+				return updatedNews;
+			});
 		},
 	});
 }
@@ -62,7 +76,7 @@ export function useMutationPatchNews() {
 
 async function patchArchive(id: string) {
 	const archive = api.patch<News>(`news/${id}/archive`);
-	
+
 	toast.promise(archive, {
 		loading: "Delete news",
 		success: "Delete news with success",
@@ -74,7 +88,7 @@ async function patchArchive(id: string) {
 		},
 	});
 
-	console.log(archive)
+	console.log(archive);
 	return await archive;
 }
 
@@ -84,12 +98,13 @@ export function useMutationPatchArchive() {
 	return useMutation({
 		mutationFn: patchArchive,
 		onSuccess: (_, variables) => {
-			queryClient.setQueryData(
-				["archiveNews", variables],
-				(data: News[]) => {
-					console.log(data)
-				}
-			);
+			queryClient.setQueryData(["news"], (data: ContentNews) => {
+				const filteredContent = data.content.filter(
+					(item) => item.id !== variables
+				);
+				const filteredData = { ...data, content: filteredContent };
+				return filteredData;
+			});
 		},
 	});
 }
