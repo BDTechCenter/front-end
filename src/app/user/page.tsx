e799168644f98deb2139890c3ca12454153be9a8
joaop-ribeiro <joao06.ribeiro.costa@gmail.com>
2024-04-22 13:20:39 -0300

    :fix: update filter

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,13 +1,14 @@
 import NavBar from "@/components/base/common/NavBar";
 import TopBanner from "@/components/base/common/TopBanner";
-import {
-	Carousel,
-	CarouselContent,
-	CarouselPrevious,
-	CarouselNext,
-} from "@/components/ui/carousel";
 import NewsUserList from "@/components/base/user/NewsUserList";
 import Footer from "@/components/base/common/Footer";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import Link from "next/link";
+import { LinkFilterNewsUser } from "@/components/base/user/LinkFilterNewsUser";
+import { RoleGuard } from "@/components/base/common/RoleGuard";
+import { FormAddNews } from "@/components/base/news/FormAddNews";
+import { appRoles } from "@/lib/sso/authConfig";
 
 export default function User() {
 	return (
@@ -16,12 +17,20 @@ export default function User() {
 			<TopBanner
 				text={dataUserPage.bannerNews}
 				className="flex w-1/2 justify-end  items-center"
-			></TopBanner>
-			<section className="my-24 mx-28 h-full 2xl:mx-44 2xl:my-36">
-				<NewsUserList
-					massageNotFound={dataUserPage.newsErrorNotFound}
-					massageError={dataUserPage.newsError}
-				/>
+			>
+				<RoleGuard roles={[appRoles.Admin, appRoles.BDUser]}>
+					<div className="flex items-center justify-center w-1/2 h-full">
+						<LinkFilterNewsUser />
+					</div>
+				</RoleGuard>
+			</TopBanner>
+			<section className="my-20 mx-28 h-full 2xl:mx-44 2xl:my-36">
+				<RoleGuard roles={[appRoles.Admin, appRoles.BDUser]}>
+					<NewsUserList
+						massageNotFound={dataUserPage.newsErrorNotFound}
+						massageError={dataUserPage.newsError}
+					/>
+				</RoleGuard>
 			</section>
 			<Footer />
 		</main>
