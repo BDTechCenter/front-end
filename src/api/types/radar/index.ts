6011eeca7f7bc55f61a91eee60b4f0a5dc160385
Flepsz <luisfelipelfsp3@gmail.com>
2024-04-25 10:54:39 -0300

    :sparkles: feat: Radar model types

--- src/api/types/radar/index.ts
+++ src/api/types/radar/index.ts
@@ -54,6 +54,10 @@ export interface TechRadarConfigData {
 	};
 }
 
+export type Quadrant = {
+	[name: string]: Item[];
+};
+
 export type QuadrantConfig = {
 	colour: string;
 	txtColour: string;
@@ -66,3 +70,50 @@ export enum HomepageOption {
 	columns = "columns",
 	both = "both",
 }
+
+export type Group = {
+	[quadrant: string]: Quadrant;
+};
+
+export interface ConfigData {
+	tags?: string[];
+	quadrants: { [key: string]: string };
+	rings: string[];
+	showEmptyRings: boolean;
+	quadrantsMap: { [quadrant: string]: QuadrantConfig };
+	chartConfig: {
+		size: number;
+		scale: number[];
+		blipSize: number;
+		ringsAttributes: { radius: number; arcWidth: number }[];
+	};
+	homepageContent: HomepageOption;
+	dateFormat?: string;
+	editLink?: {
+		radarLink: string;
+		title?: string;
+	};
+}
+
+export enum Language {
+	en = "en",
+}
+
+export enum StorageKey {
+	language = "language",
+}
+
+export type RingName = "all" | "adopt" | "trial" | "assess" | "hold"
+
+export type Blip = Item & {
+  quadrantPosition: number;
+  ringPosition: number;
+  colour: string;
+  txtColour: string;
+  coordinates: Point;
+};
+
+export type Point = {
+  x: number;
+  y: number;
+};
