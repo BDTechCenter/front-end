7e47d6ec12f7159405286271f51f3db8b2bbe2b3
joaop-ribeiro <joao06.ribeiro.costa@gmail.com>
2024-05-29 09:27:30 -0300

    :fix: update responsive design comments image error

--- src/components/base/common/ImageError.tsx
+++ src/components/base/common/ImageError.tsx
@@ -8,13 +8,13 @@ interface ImageErrorProps {
 export default function ImageError({ data }: ImageErrorProps) {
 	return (
 		<div className="flex w-full h-full my-10 flex-col items-center justify-center">
-			<div className="w-1/3 f-1/3">
+			<div className="flex justify-center items-center w-full 2xl:w-1/2 h-full">
 				<Image
-					className="w-full h-full"
+					className="w-1/2 h-full"
 					src={data.img}
 					alt="Erro"
-					height="800"
-					width="800"
+					height="500"
+					width="500"
 				/>
 			</div>
 			<h1 className="text-xl 2xl:text-2xl font-semibold text-bdlightpurple">
